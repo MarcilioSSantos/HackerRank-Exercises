1eaab33e1bcf13607172641d6ec39c39562acb06
MarcilioSSantos <ssantossilvamarcilio@gmail.com>
2021-08-24 09:17:16 -0300

    add final solution to "countValleys" rename preview file

---
+++ interview-preparation-kit/countValleys.js
@@ -0,0 +1,14 @@
+const compareArray = (steps,path) => {
+
+    let valleys = 0;
+    let elevation = 0;
+    for(let i=0;i<steps;i++){
+        if(path[i] == 'D'){
+            elevation--;
+        }else{
+            elevation == -1 ? valleys++ : 0;
+            elevation++;
+        }
+    }
+    return valleys;
+}
